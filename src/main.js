32496e17065ec7823ed08061731008cbf5b3628a
vasusharma960 <vasusharma.2058@gmail.com>
2023-03-05 12:56:52 +0530

    Render bugs fixed, post with sample api

--- src/main.js
+++ src/main.js
@@ -1,130 +1,70 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import toast from 'react-hot-toast';
 import Result from './result';
+//67467f92-a588-4b1e-b4e6-011ca0835834
 
 export default function Main(){
+  const [initialRender, setInitialRender] = useState(true);
   const [url, setURL] = useState('');
   const [temp, setTemp] = useState('');
-  const [data, setData] = useState({
-    "domain_name": "AMAZON.COM",
-    "registrar": "MarkMonitor Inc.",
-    "whois_server": "whois.markmonitor.com",
-    "referral_url": null,
-    "updated_date": "2019-05-07T20:09:37",
-    "creation_date": "1994-11-01T05:00:00",
-    "expiration_date": "2024-10-31T04:00:00",
-    "emails": "abusecomplaints@markmonitor.com",
-    "dnssec": "unsigned",
-    "name": null,
-    "org": null,
-    "address": null,
-    "city": null,
-    "state": null,
-    "registrant_postal_code": null,
-    "country": null,
-    "URL": "https://aws.amazon.com/certification/",
-    "url_len": 37,
-    "domain": "aws.amazon.com",
-    "subDomain": "aws",
-    "subDomain_count": 1,
-    "@": 0,
-    "?": 0,
-    "-": 0,
-    "=": 0,
-    ".": 2,
-    "#": 0,
-    "%": 0,
-    "+": 0,
-    "$": 0,
-    "!": 0,
-    "*": 0,
-    ",": 0,
-    "//": 1,
-    "abnormal_url": 1,
-    "https": 1,
-    "digits": 0,
-    "letters": 30,
-    "Shortining_Service": 0,
-    "having_ip_address": 0,
-    "large_url": 0,
-    "long_url": 0,
-    "short_url": 1,
-    "Domain_part_count": 3,
-    "fld_lenght": 14,
-    "topLevel_domain": "com",
-    "tld_count": 3,
-    "url_path_len": 27,
-    "count_PathDir": 4,
-    "first_dir_len": 0,
-    "sus_url": 0,
-    "hostname_length": 14,
-    "url_alphas": 30,
-    "url_digits": 0,
-    "url_puncs": 7,
-    "pc_alphas": 0.8108108108108109,
-    "pc_digits": 0.0,
-    "pc_puncs": 0.1891891891891892,
-    "url_path": "/certification/",
-    "get_url_path_len": 15,
-    "subD_len": 3,
-    "has_favicon": true,
-    "result": "safe",
-    "feature_count": 66
-});
+  const [data, setData] = useState({});
 
 const [result, setResult] = useState([]);
 const [showData, setShow] = useState(false);
 const [color, setColor] = useState('red');
 
 useEffect(() => {
+  if(!initialRender){
   Object.entries(data).forEach((entry) => {
     const [key, value] = entry;
+    console.log(`${key}: ${value}`);
     setResult((prevstate) => {
       return [...prevstate, `${key}: ${value}`]
     })
-  });
 
-  if(data.result === "safe"){
-    setColor('green');
+    if(data.result === "safe"){
+      setColor('green');
+    }
+  });
+  setShow(true);
+  toast.success();
+}
+}, [data])
+
+useEffect(() => {
+  if(initialRender){
+    setInitialRender(false);
+    return;
   }
-}, [])
+  try{
+    console.log(url);
+    axios.post('https://getpantry.cloud/apiv1/pantry/67467f92-a588-4b1e-b4e6-011ca0835834/basket/UR', {"a": url})
+    .then(res => setData(res.data));
+  }catch(err){
+    toast.error(err);
+  }
+}, [url]);
 
   function handleChange(event){
     setTemp(event.target.value);
   }
-  const handleClick = async (event) => {
+
+  function handleClick(event){
     event.preventDefault();
-    setShow(true);
 
-    // try {
-    //   const response = await fetch('https://reqres.in/api/users', {
-    //     method: 'GET',
-    //     headers: {
-    //       Accept: 'application/json',
-    //     },
-    //   });
-    //
-    //   if (!response.ok) {
-    //     throw new Error(`Error! status: ${response.status}`);
-    //   }
-    //
-    //   const result = await response.json();
-    //
-    //
-    //
-    //   setURL(JSON.parse(JSON.stringify(result)));
-    //   setShow(true);
-    // } catch (err) {
-    //   toast.error(err);
-    // }
-
-
-    toast.success(temp);
+    if(temp.length === 0){
+      toast.error("Enter URL");
+    }
+    setURL(temp);
   }
 
   function handleClear(event){
     event.preventDefault();
 
+    setURL('');
+    setTemp('')
+    setInitialRender(true);
     setShow(false);
   }
   return (
